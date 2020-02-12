12cab14fa8710363734d2cd814b3a9e290950039
jonapang13 <pangeli8@seattleu.edu>
2020-02-12 15:24:05 -0800

    Added IndexAsync to DatabaseService

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -102,5 +102,14 @@ namespace Mine.Services
 
             return (result == 1);
         }
+
+        /// <summary>
+        /// Return all records in the database
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<ItemModel>> IndexAsync()
+        {
+            return await Database.Table<ItemModel>().ToListAsync();
+        }
     }
 }
